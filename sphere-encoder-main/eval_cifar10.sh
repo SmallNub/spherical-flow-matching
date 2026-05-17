c5957299559582876b1c37bc2f6a7bfdc3ca7f01
nordin-26 <nordinelassassi@hotmail.com>
2026-05-17 18:28:38 +0200

    adding extra meassures

--- sphere-encoder-main/eval_cifar10.sh
+++ sphere-encoder-main/eval_cifar10.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python eval_encodings.py \
+python eval_quality_diversity.py \
   --encoding_path workspace/experiments/sphere-small-small-cifar-10-32px/encoding/output_encodings.npz \
   --checkpoint workspace/experiments/sphere-small-small-cifar-10-32px/ckpt \
   --output_dir workspace/experiments/sphere-small-small-cifar-10-32px/decoded_eval \
   --dataset_name cifar-10 \
-  --batch_size 128 \
+  --batch_size 16 \
   --use_ema True \
   --dtype bfloat16 \
   --normalize_latents True \
@@ -13,4 +13,7 @@ python eval_encodings.py \
   --save_images False \
   --seed 42 \
   --deterministic False \
-  --compile_model True
+  --compile_model True \
+  --nearest_k 5 \
+  --use_isc False \
+  --metrics_output workspace/experiments/sphere-small-small-cifar-10-32px/decoded_eval/metrics_summary.json
